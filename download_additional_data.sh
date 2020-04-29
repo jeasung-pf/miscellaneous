1c7af2034fccfce7504006f54ed5f32d3833fa3c
admin <admin@admins-MacBook-Pro-2.local>
2020-04-29 16:43:45 +0900

    First commit

--- download_additional_data.sh
+++ download_additional_data.sh
@@ -1,1 +1,1 @@
-wget https://drive.google.com/uc?export=download&id=1DAcIy13-Xnutww4nJjZxekSVRUJaYxWa
+wget https://www.googleapis.com/drive/v3/files/1DAcIy13-Xnutww4nJjZxekSVRUJaYxWa?alt=media&key=AIzaSyDBZ1AkUTKf_jMxwBe9NtLwy_lmBByvrw4
